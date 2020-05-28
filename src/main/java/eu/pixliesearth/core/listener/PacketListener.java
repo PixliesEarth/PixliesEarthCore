d66685b2a93c7bbabe46a942762f96236e9d6339
Machmeinparaillegal <62360252+Machmeinparaillegal@users.noreply.github.com>
2020-05-29 00:14:32 +0200

    Removed debug lines

--- src/main/java/eu/pixliesearth/core/listener/PacketListener.java
+++ src/main/java/eu/pixliesearth/core/listener/PacketListener.java
@@ -25,16 +25,14 @@ public class PacketListener  implements Listener{
     public void onPacket(ProjectileLaunchEvent e){
 
         if(!(e.getEntity() instanceof Arrow)) return;
-        System.out.println("REACHED ENTITY SPAWN EVENT\nREACHED ENTITY SPAWN EVENT");
-        System.out.println(e.getEntity().getCustomName());
-        System.out.println(e.getEntity().getShooter());
+
+
         protocolManager = ProtocolLibrary.getProtocolManager();
         protocolManager.addPacketListener(new PacketAdapter(Main.getInstance(), ListenerPriority.NORMAL, PacketType.Play.Server.SPAWN_ENTITY) {
             @Override
             public void onPacketSending(PacketEvent event) {
                 //super.onPacketSending(event);
                 if(event.getPacketType() == PacketType.Play.Server.SPAWN_ENTITY){
-                    System.out.println("REACHED\nREACHED\nREACHED\nREACHED");
                     if(e.getEntity().getCustomName() == null) return;
                     if(e.getEntity().getCustomName().equalsIgnoreCase("§c7.62mm")) {
                         event.setCancelled(true);
