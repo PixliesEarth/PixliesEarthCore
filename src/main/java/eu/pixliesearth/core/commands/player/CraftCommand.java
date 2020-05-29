4988beed38eaa9adb77c1fc00134a249c1f7dd7b
MickMMars <a.khamisipour@outlook.de>
2020-05-30 00:20:49 +0200

    /craft & /enderchest

---
+++ src/main/java/eu/pixliesearth/core/commands/player/CraftCommand.java
@@ -0,0 +1,26 @@
+package eu.pixliesearth.core.commands.player;
+
+import eu.pixliesearth.localization.Lang;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CraftCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Lang.ONLY_PLAYERS_EXEC.get(sender));
+            return false;
+        }
+        Player player = (Player) sender;
+        if (!player.hasPermission("earth.craft")) {
+            player.sendMessage(Lang.NO_PERMISSIONS.get(player));
+            return false;
+        }
+        player.openWorkbench(null, true);
+        return false;
+    }
+
+}
