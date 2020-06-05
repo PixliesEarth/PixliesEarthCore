c286e133a3c42b38a262dc1e06bd882117346456
MickMMars <a.khamisipour@outlook.de>
2020-06-05 03:59:08 +0200

    Fixed MiniMick bugs

--- src/main/java/eu/pixliesearth/core/listener/AchievementListener.java
+++ src/main/java/eu/pixliesearth/core/listener/AchievementListener.java
@@ -16,15 +16,19 @@ import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class AchievmentListener implements Listener {
+public class AchievementListener implements Listener {
+
     @EventHandler
-    public void onAchievment(PlayerAdvancementDoneEvent e){
+    public void onAchievement(PlayerAdvancementDoneEvent e) {
         Player p = e.getPlayer();
         Advancement a = e.getAdvancement();
         String rawAdvancementName = a.getKey().getKey();
-        String advancementName = Arrays.stream(rawAdvancementName.substring(rawAdvancementName.lastIndexOf("/") + 1).toLowerCase().split("_"))
-                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
-                .collect(Collectors.joining(" "));
-        MiniMick.getApi().getServerTextChannelById(Main.getInstance().getConfig().getString("chatchannel")).get().sendMessage(ChatColor.stripColor( "**" + PlaceholderAPI.setPlaceholders(p, "%vault_prefix%" + p.getDisplayName()) + " has made the advancement " + advancementName + "!**"));
+        if (!a.getKey().toString().startsWith("minecraft:recipes")) {
+            String advancementName = Arrays.stream(rawAdvancementName.substring(rawAdvancementName.lastIndexOf("/") + 1).toLowerCase().split("_"))
+                    .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                    .collect(Collectors.joining(" "));
+            MiniMick.getApi().getServerTextChannelById(Main.getInstance().getConfig().getString("chatchannel")).get().sendMessage(ChatColor.stripColor("\uD83C\uDF8A **" + PlaceholderAPI.setPlaceholders(p, "%vault_prefix%" + p.getDisplayName()) + " has made the advancement " + advancementName + "!**"));
+        }
     }
+
 }
