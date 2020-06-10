5b25680677638c35505919192ecaf6a7bd055b83
MickMMars <a.khamisipour@outlook.de>
2020-06-10 23:56:09 +0200

    Added BlockBreakListener

--- src/main/java/eu/pixliesearth/core/customitems/listeners/ItemsInteractEvent.java
+++ src/main/java/eu/pixliesearth/core/customitems/listeners/ItemsInteractEvent.java
@@ -19,18 +19,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ItemsInteractEvent implements Listener {
+
     @EventHandler
-    public void onInteract(PlayerInteractEvent event){
-        if(event.getAction().equals(Action.LEFT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_AIR)){
-            if(event.getPlayer().getInventory().getItemInMainHand().getType() == Material.STICK){
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getAction().equals(Action.LEFT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_AIR)) {
+            if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.STICK) {
 
-            if(event.getPlayer().getInventory().getItemInMainHand().getItemMeta() != null) {
+                if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta() != null) {
 
-                if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore() != null) {
+                    if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore() != null) {
 
-                    if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore().get(1).equals(new ItemSlingshot().getLore().get(1))) {
+                        if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore().get(1).equals(new ItemSlingshot().getLore().get(1))) {
 
-                        if (event.getPlayer().getInventory().contains(Material.COBBLESTONE) || event.getPlayer().getInventory().contains(Material.STONE) || event.getPlayer().getInventory().contains(Material.GRAVEL)) {
+                            if (event.getPlayer().getInventory().contains(Material.COBBLESTONE) || event.getPlayer().getInventory().contains(Material.STONE) || event.getPlayer().getInventory().contains(Material.GRAVEL)) {
 
 
                         /*ItemStack arrow = new ItemStack(Material.ARROW);
@@ -41,28 +42,27 @@ public class ItemsInteractEvent implements Listener {
                         arrow.setItemMeta(meta);
                         ((CraftPlayer) event.getPlayer()).getHandle().playerConnection.sendPacket(new PacketPlayOutSetSlot(0, 9, CraftItemStack.asNMSCopy(arrow)));
                         */
-                            if (!(Main.getInstance().getUtilLists().reloading.isEmpty())) {
-                                if (Main.getInstance().getUtilLists().reloading.contains(event.getPlayer().getUniqueId())) {
-                                    event.getPlayer().sendActionBar(Lang.CUSTOM_SLINGSHOT_RELOADING_ACTIONBAR.get(event.getPlayer()));
-                                    return;
+                                if (!(Main.getInstance().getUtilLists().reloading.isEmpty())) {
+                                    if (Main.getInstance().getUtilLists().reloading.contains(event.getPlayer().getUniqueId())) {
+                                        event.getPlayer().sendActionBar(Lang.CUSTOM_SLINGSHOT_RELOADING_ACTIONBAR.get(event.getPlayer()));
+                                        return;
+                                    }
                                 }
-                            }
-                            String[] splitted =  event.getPlayer().getInventory().getItemInMainHand().getLore().get(2).split(":");
-                            String predurability = splitted[1].replace(" ", "");
-                            int durability = Integer.parseInt(predurability);
+                                String[] splitted = event.getPlayer().getInventory().getItemInMainHand().getLore().get(2).split(":");
+                                String predurability = splitted[1].replace(" ", "");
+                                int durability = Integer.parseInt(predurability);
 
-                            int newDurability = durability - 1;
-                            if(newDurability == 0){
-                                removeOne(event.getPlayer().getInventory().getItemInMainHand(), event.getPlayer());
-                            }else {
-                                ArrayList<String> lore = new ArrayList<String>();
-                                lore.add(new ItemSlingshot().getLore().get(0));
-                                lore.add(new ItemSlingshot().getLore().get(1));
-                                lore.add(splitted[0] + ": " + newDurability);
-                                event.getPlayer().getInventory().getItemInMainHand().setLore(lore);
-                            }
-                            Bukkit.getPluginManager().callEvent(new SlingShotEvent(event.getPlayer(), "SnowStone"));
-
+                                int newDurability = durability - 1;
+                                if (newDurability == 0) {
+                                    removeOne(event.getPlayer().getInventory().getItemInMainHand(), event.getPlayer());
+                                } else {
+                                    ArrayList<String> lore = new ArrayList<String>();
+                                    lore.add(new ItemSlingshot().getLore().get(0));
+                                    lore.add(new ItemSlingshot().getLore().get(1));
+                                    lore.add(splitted[0] + ": " + newDurability);
+                                    event.getPlayer().getInventory().getItemInMainHand().setLore(lore);
+                                }
+                                Bukkit.getPluginManager().callEvent(new SlingShotEvent(event.getPlayer(), "SnowStone"));
 
 
                             }
@@ -72,13 +72,14 @@ public class ItemsInteractEvent implements Listener {
             }
         }
     }
-    public void removeOne(ItemStack i, Player p){
+
+    public void removeOne(ItemStack i, Player p) {
         HashMap<Integer, ? extends ItemStack> map = p.getInventory().all(i);
-        if(map.isEmpty()){
+        if (map.isEmpty()) {
             return;
         }
-        for(Map.Entry<Integer, ? extends ItemStack> entry : map.entrySet())
-            if(entry.getValue().getAmount() != 0) {
+        for (Map.Entry<Integer, ? extends ItemStack> entry : map.entrySet())
+            if (entry.getValue().getAmount() != 0) {
                 if (entry.getValue().getAmount() == 1) p.getPlayer().getInventory().setItem(entry.getKey(), null);
                 else {
                     entry.getValue().setAmount(entry.getValue().getAmount() - 1);
---
+++ src/main/java/eu/pixliesearth/core/listener/BlockBreakListener.java
@@ -0,0 +1,134 @@
+package eu.pixliesearth.core.listener;
+
+import eu.pixliesearth.core.customitems.ci.ItemExplosivePick;
+import eu.pixliesearth.localization.Lang;
+import eu.pixliesearth.utils.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+public class BlockBreakListener implements Listener {
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent event) {
+        //Explosive Pick
+        if(!event.isCancelled()) {
+            if (event.getPlayer().getInventory().getItemInMainHand() != null) {
+                if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.DIAMOND_PICKAXE) {
+                    if (event.getPlayer().getInventory().getItemInMainHand().hasItemMeta()) {
+                        if (event.getPlayer().getInventory().getItemInMainHand().getItemMeta().getLore() == new ItemExplosivePick().getLore()) {
+                            Block b = event.getBlock();
+                            if (b.getType() != Material.TORCH
+                                    && b.getType() != Material.REDSTONE_TORCH
+                                    && b.getType() != Material.REDSTONE_WALL_TORCH
+                                    && b.getType() != Material.WALL_TORCH
+                                    && b.getType() != Material.END_ROD
+                                    && b.getType() != Material.TRIPWIRE_HOOK) {
+                                ArrayList<Block> blocks = new ArrayList<>();
+
+
+                                Block up = b.getRelative(BlockFace.UP, 1);
+                                Block down = b.getRelative(BlockFace.DOWN, 1);
+                                Block right = b.getRelative(BlockFace.EAST, 1);
+                                Block left = b.getRelative(BlockFace.WEST, 1);
+                                Block south = b.getRelative(BlockFace.SOUTH, 1);
+                                Block north = b.getRelative(BlockFace.NORTH, 1);
+                                Block downeast = down.getRelative(BlockFace.EAST, 1);
+                                Block downwest = down.getRelative(BlockFace.WEST, 1);
+                                Block downsouth = down.getRelative(BlockFace.SOUTH, 1);
+                                Block downnorth = down.getRelative(BlockFace.NORTH, 1);
+                                Block upeast = up.getRelative(BlockFace.EAST, 1);
+                                Block upwest = up.getRelative(BlockFace.WEST, 1);
+                                Block upsouth = up.getRelative(BlockFace.SOUTH, 1);
+                                Block upnorth = up.getRelative(BlockFace.NORTH, 1);
+                                Block downsoutheast = downsouth.getRelative(BlockFace.EAST, 1);
+                                Block downsouthwest = downsouth.getRelative(BlockFace.WEST, 1);
+                                Block downnorthwest = downnorth.getRelative(BlockFace.WEST, 1);
+                                Block downnortheast = downnorth.getRelative(BlockFace.EAST, 1);
+                                Block southeast = south.getRelative(BlockFace.EAST, 1);
+                                Block southwest = south.getRelative(BlockFace.WEST, 1);
+                                Block northeast = north.getRelative(BlockFace.EAST, 1);
+                                Block northwest = north.getRelative(BlockFace.WEST, 1);
+                                Block upsoutheast = upsouth.getRelative(BlockFace.EAST, 1);
+                                Block upsouthwest = upsouth.getRelative(BlockFace.WEST, 1);
+                                Block upnorthwest = upnorth.getRelative(BlockFace.WEST, 1);
+                                Block upnortheast = upnorth.getRelative(BlockFace.EAST, 1);
+
+                                blocks.add(b);
+                                blocks.add(up);
+                                blocks.add(down);
+                                blocks.add(right);
+                                blocks.add(left);
+                                blocks.add(south);
+                                blocks.add(north);
+                                blocks.add(downeast);
+                                blocks.add(downwest);
+                                blocks.add(downsouth);
+                                blocks.add(downnorth);
+                                blocks.add(upeast);
+                                blocks.add(upwest);
+                                blocks.add(upnorth);
+                                blocks.add(upsouth);
+                                blocks.add(downsoutheast);
+                                blocks.add(downsouthwest);
+                                blocks.add(downnorthwest);
+                                blocks.add(downnortheast);
+                                blocks.add(southeast);
+                                blocks.add(southwest);
+                                blocks.add(northeast);
+                                blocks.add(northwest);
+                                blocks.add(upsoutheast);
+                                blocks.add(upsouthwest);
+                                blocks.add(upnorthwest);
+                                blocks.add(upnortheast);
+
+                                event.getPlayer().getWorld().playSound(event.getPlayer().getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 0.2F, 1F);
+                                event.getPlayer().getWorld().createExplosion(event.getBlock().getLocation(), 0.0F);
+                                for (Block block : blocks) {
+                                    if (block.getType() != Material.BEDROCK
+                                            && block.getType() != Material.COMMAND_BLOCK
+                                            && block.getType() != Material.CHAIN_COMMAND_BLOCK
+                                            && block.getType() != Material.REPEATING_COMMAND_BLOCK
+                                            && block.getType() != Material.WATER
+                                            && block.getType() != Material.LAVA
+                                            && block.getType() != Material.BARRIER
+                                            && block.getType() != Material.ARMOR_STAND) {
+                                        block.breakNaturally(event.getPlayer().getInventory().getItemInMainHand());
+                                        block.getWorld().playEffect(block.getLocation(), Effect.STEP_SOUND, block.getType());
+                                    }
+                                }
+                            }else{
+                                event.setCancelled(true);
+                                event.getBlock().breakNaturally(event.getPlayer().getInventory().getItemInMainHand());
+                                event.getPlayer().sendMessage(Lang.VERY_SMART.get(event.getPlayer()));
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        //Mick stuff
+        if (event.getBlock().getType().equals(Material.IRON_ORE) || event.getBlock().getType().equals(Material.DIAMOND_ORE) || event.getBlock().getType().equals(Material.GOLD_ORE)) {
+            Random r = new Random();
+            if (r.nextDouble() < 0.25) {
+                event.getBlock().getDrops().add(new ItemBuilder(Material.POTION).setDisplayName("§e§lENERGY BOTTLE §8(§f§l2§8)").addLoreLine("§7§oRight-click to use").build());
+            }
+        }
+    }
+
+}
