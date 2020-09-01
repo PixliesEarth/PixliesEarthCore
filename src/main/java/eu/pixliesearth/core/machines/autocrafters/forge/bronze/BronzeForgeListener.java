c10b9e6c27297218b20de724ab83f24589d48c09
Amirmahdi Khamisipour <a.khamisipour@outlook.de>
2020-09-01 21:25:24 +0200

    Fixed some stuf

--- src/main/java/eu/pixliesearth/core/machines/autocrafters/forge/bronze/BronzeForgeListener.java
+++ src/main/java/eu/pixliesearth/core/machines/autocrafters/forge/bronze/BronzeForgeListener.java
@@ -31,29 +31,10 @@ public class BronzeForgeListener implements Listener {
     }
 
     @EventHandler
-    public void onRightClick(PlayerInteractEvent event) {
-        if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
-        Block b = event.getClickedBlock();
-        if (b == null) return;
-        if (!instance.getUtilLists().machines.containsKey(b.getLocation())) return;
-        instance.getUtilLists().openMachines.put(event.getPlayer().getUniqueId(), instance.getUtilLists().machines.get(b.getLocation()));
-        instance.getUtilLists().machines.get(b.getLocation()).open(event.getPlayer());
-    }
-
-    @EventHandler
     public void onPlace(BlockPlaceEvent event) {
         ItemStack item = event.getItemInHand();
         if (!item.isSimilar(BronzeForge.item)) return;
         instance.getUtilLists().machines.put(event.getBlockPlaced().getLocation(), new BronzeForge(Methods.generateId(7), event.getBlockPlaced().getLocation()));
-    }
-
-    @EventHandler
-    public void onBreak(BlockBreakEvent event) {
-        if (!instance.getUtilLists().machines.containsKey(event.getBlock().getLocation())) return;
-        event.setDropItems(false);
-        Machine mill = instance.getUtilLists().machines.get(event.getBlock().getLocation());
-        mill.getLocation().getWorld().dropItemNaturally(mill.getLocation(), mill.getItem());
-        mill.remove();
     }
 
     @EventHandler
