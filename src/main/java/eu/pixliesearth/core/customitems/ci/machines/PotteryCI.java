eeb3f2128730cbe48fe964039afc84ed6ab11bef
Amirmahdi Khamisipour <a.khamisipour@outlook.de>
2020-09-11 02:06:12 +0200

    /cigive now supports machines

---
+++ src/main/java/eu/pixliesearth/core/customitems/ci/machines/PotteryCI.java
@@ -0,0 +1,42 @@
+package eu.pixliesearth.core.customitems.ci.machines;
+
+import eu.pixliesearth.core.customitems.CustomItem;
+import eu.pixliesearth.core.machines.Machine;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class PotteryCI implements CustomItem {
+
+    @Override
+    public ItemStack getItem() {
+        return Machine.MachineType.POTTERY.getItem();
+    }
+
+    @Override
+    public String getName() {
+        return getItem().getItemMeta().getDisplayName();
+    }
+
+    @Override
+    public List<String> getLore() {
+        return getItem().getLore();
+    }
+
+    @Override
+    public ItemStack getStatic(int durability) {
+        return getItem();
+    }
+
+    @Override
+    public boolean enchantable() {
+        return false;
+    }
+
+    @Override
+    public void onInteract(PlayerInteractEvent event) {
+
+    }
+
+}
