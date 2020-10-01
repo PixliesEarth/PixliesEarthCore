e25c671ad91b6dfdb0eb22cb37959977db6da8c3
BradB <45739045+BradBot1@users.noreply.github.com>
2020-10-01 20:20:29 +0100

    a

--- src/main/java/eu/pixliesearth/core/customblocks/blocks/BronzeBlock.java
+++ src/main/java/eu/pixliesearth/core/customblocks/blocks/BronzeBlock.java
@@ -1,18 +1,22 @@
 package eu.pixliesearth.core.customblocks.blocks;
 
-import org.bukkit.Material;
+import java.util.UUID;
+
+import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
 
 import eu.pixliesearth.core.customblocks.CustomBlock;
-import eu.pixliesearth.utils.ItemBuilder;
+import eu.pixliesearth.core.customitems.ci.blocks.BlockBronze;
 
 public class BronzeBlock extends CustomBlock {
 	
-	public static ItemStack item = new ItemBuilder(Material.GOLD_BLOCK).setDisplayName(new BronzeBlock().getTitle()).setCustomModelData(25).build();
+	public BronzeBlock(UUID id, Location location) {
+		super(id, location);
+	}
 	
 	@Override
 	public ItemStack getItem() {
-		return item;
+		return BlockBronze.item;
 	}
 	
 	@Override
