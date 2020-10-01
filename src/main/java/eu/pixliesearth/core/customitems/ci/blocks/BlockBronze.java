889feedb8b89a86a9516aee371e1d0d4bde0ad63
BradB <45739045+BradBot1@users.noreply.github.com>
2020-10-01 20:28:17 +0100

    e

--- src/main/java/eu/pixliesearth/core/customitems/ci/blocks/BlockBronze.java
+++ src/main/java/eu/pixliesearth/core/customitems/ci/blocks/BlockBronze.java
@@ -6,43 +6,46 @@ import org.bukkit.Material;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-import eu.pixliesearth.core.customblocks.blocks.BronzeBlock;
 import eu.pixliesearth.core.customitems.CustomItem;
 import eu.pixliesearth.utils.ItemBuilder;
 
 public class BlockBronze implements CustomItem {
+	
+	public static ItemStack item = new ItemBuilder(Material.GOLD_BLOCK).setDisplayName(new BlockBronze().getTitle()).setCustomModelData(25).build();
+	
+	public String getTitle() {
+		return "§6BronzeBlock";
+	}
+	
+	@Override
+	public ItemStack getItem() {
+		return item;
+	}
 
-    public static ItemStack item = new ItemBuilder(Material.GOLD_BLOCK).setDisplayName(new BronzeBlock().getTitle()).setCustomModelData(25).build();
+	@Override
+	public String getName() {
+		return getItem().getItemMeta().getDisplayName();
+	}
 
-    @Override
-    public ItemStack getItem() {
-        return BlockBronze.item;
-    }
-
-    @Override
-    public String getName() {
-        return getItem().getItemMeta().getDisplayName();
-    }
-
-    @Override
-    public List<String> getLore() {
-        List<String> lore = getItem().getItemMeta().getLore();
+	@Override
+	public List<String> getLore() {
+		List<String> lore = getItem().getItemMeta().getLore();
         return lore;
-    }
+	}
 
-    @Override
-    public ItemStack getStatic(int durability) {
-        return item;
-    }
+	@Override
+	public ItemStack getStatic(int durability) {
+		return item;
+	}
 
-    @Override
-    public boolean enchantable() {
-        return false;
-    }
+	@Override
+	public boolean enchantable() {
+		return false;
+	}
 
-    @Override
-    public void onInteract(PlayerInteractEvent event)
-    {
-    }
+	@Override
+	public void onInteract(PlayerInteractEvent event) 
+	{
+	}
 
 }
