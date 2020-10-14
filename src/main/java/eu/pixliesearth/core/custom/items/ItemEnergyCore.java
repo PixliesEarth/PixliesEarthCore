c1707422ca9e4d502aa749d7337bc42a2b097c5d
BradBot1 <Bradley6733@outlook.com>
2020-10-14 18:24:08 +0100

    items

---
+++ src/main/java/eu/pixliesearth/core/custom/items/ItemEnergyCore.java
@@ -0,0 +1,84 @@
+package eu.pixliesearth.core.custom.items;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
+
+import eu.pixliesearth.core.custom.CustomItem;
+
+public class ItemEnergyCore extends CustomItem {
+	
+    public ItemEnergyCore() {
+
+    }
+
+    @Override
+    public Material getMaterial() {
+        return Material.NETHER_STAR;
+    }
+
+    @Override
+    public List<String> getDefaultLore() {
+        return new ArrayList<String>() {/** */private static final long serialVersionUID = 7541112248281122525L;{
+        	add("§c§lExtremely explosive!");
+        }};
+    }
+
+    @Override
+    public String getDefaultDisplayName() {
+        return "§6Unprotected Energy Core";
+    }
+
+    @Override
+    public boolean isGlowing() {
+        return false;
+    }
+
+    @Override
+    public boolean isUnbreakable() {
+        return false;
+    }
+
+    @Override
+    public Map<String, Object> getDefaultNBT() {
+        return new HashMap<String, Object>();
+    }
+
+    @Override
+    public Map<Enchantment, Integer> getDefaultEnchants() {
+        return new HashMap<Enchantment, Integer>();
+    }
+
+    @Override
+    public Set<ItemFlag> getItemFlags(){
+        return new HashSet<ItemFlag>();
+    }
+
+    @Override
+    public Integer getCustomModelData() {
+        return null;
+    }
+
+    @Override
+    public CreativeTabs getCreativeTab() {
+        return CreativeTabs.MISC;
+    }
+
+    @Override
+    public String getUUID() {
+        return "Pixlies:Energy_Core_Unprotected"; // 6bcc41e5-5a09-4955-8756-f06c26d61c4d
+    }
+
+    @Override
+    public boolean PlayerInteractEvent(PlayerInteractEvent event) {
+        return false;
+    }
+}
