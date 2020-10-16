b3ab52c45e8bf6861312a1eebf6f2fb03fc2d152
BradBot1 <Bradley6733@outlook.com>
2020-10-16 15:34:07 +0100

    Armour

---
+++ src/main/java/eu/pixliesearth/core/custom/items/ArmourDamascusSteelHelmet.java
@@ -0,0 +1,101 @@
+package eu.pixliesearth.core.custom.items;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
+
+import eu.pixliesearth.core.custom.CustomArmour;
+
+/**
+ * @author Zenake
+ */
+public class ArmourDamascusSteelHelmet extends CustomArmour {
+
+    public ArmourDamascusSteelHelmet() {
+
+    }
+
+    @Override
+    public Material getMaterial() {
+        return Material.IRON_HELMET;
+    }
+
+    @Override
+    public List<String> getDefaultLore() {
+        return null;
+    }
+
+    @Override
+    public String getDefaultDisplayName() {
+        return "§7§lDamascus Steel Helmet";
+    }
+
+    @Override
+    public boolean isGlowing() {
+        return false;
+    }
+
+    @Override
+    public boolean isUnbreakable() {
+        return false;
+    }
+
+    @Override
+    public Map<String, Object> getDefaultNBT() {
+        return new HashMap<String, Object>();
+    }
+
+    @Override
+    public Map<Enchantment, Integer> getDefaultEnchants() {
+        return new HashMap<Enchantment, Integer>();
+    }
+
+    @Override
+    public Set<ItemFlag> getItemFlags(){
+        return new HashSet<ItemFlag>();
+    }
+
+    @Override
+    public Integer getCustomModelData() {
+        return null;
+    }
+
+    @Override
+    public double getArmour() {
+        return 3D;
+    }
+
+    @Override
+    public double getArmourToughness() {
+        return 2.0;
+    }
+
+    @Override
+    public CreativeTabs getCreativeTab() {
+        return CreativeTabs.COMBAT;
+    }
+
+    @Override
+    public String getUUID() {
+        return "Pixlies:Damascus_Steel_Helmet"; // 6bcc41e5-5a09-4955-8756-f06c26d61c4d
+    }
+
+    @Override
+    public boolean PlayerInteractEvent(PlayerInteractEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean EntityDamageEvent(EntityDamageEvent event) {
+        return false;
+    }
+
+}
