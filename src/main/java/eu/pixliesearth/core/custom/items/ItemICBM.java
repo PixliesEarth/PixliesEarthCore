82610c75414c7bf3001da1f7526906287bce13e1
BradBot1 <Bradley6733@outlook.com>
2020-10-18 19:04:40 +0100

    missile

---
+++ src/main/java/eu/pixliesearth/core/custom/items/ItemICBM.java
@@ -0,0 +1,81 @@
+package eu.pixliesearth.core.custom.items;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
+
+import eu.pixliesearth.core.custom.CustomItem;
+
+public class ItemICBM extends CustomItem {
+	
+    public ItemICBM() {
+
+    }
+
+    @Override
+    public Material getMaterial() {
+        return Material.STICK;
+    }
+
+    @Override
+    public List<String> getDefaultLore() {
+        return null;
+    }
+
+    @Override
+    public String getDefaultDisplayName() {
+        return "§cICBM Launcher";
+    }
+
+    @Override
+    public boolean isGlowing() {
+        return false;
+    }
+
+    @Override
+    public boolean isUnbreakable() {
+        return false;
+    }
+
+    @Override
+    public Map<String, Object> getDefaultNBT() {
+        return new HashMap<String, Object>();
+    }
+
+    @Override
+    public Map<Enchantment, Integer> getDefaultEnchants() {
+        return new HashMap<Enchantment, Integer>();
+    }
+
+    @Override
+    public Set<ItemFlag> getItemFlags(){
+        return new HashSet<ItemFlag>();
+    }
+
+    @Override
+    public Integer getCustomModelData() {
+        return null;
+    }
+
+    @Override
+    public CreativeTabs getCreativeTab() {
+        return CreativeTabs.MISC;
+    }
+
+    @Override
+    public String getUUID() {
+        return "Pixlies:ICBM_Key"; // 6bcc41e5-5a09-4955-8756-f06c26d61c4d
+    }
+
+    @Override
+    public boolean PlayerInteractEvent(PlayerInteractEvent event) {
+        return false;
+    }
+}
