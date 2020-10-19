edb328dc380bc2edae19852ed09f55973b700903
Amirmahdi Khamisipour <a.khamisipour@outlook.de>
2020-10-19 15:45:05 +0200

    NationXP stuff

--- src/main/java/eu/pixliesearth/core/custom/listeners/NationXPListener.java
+++ src/main/java/eu/pixliesearth/core/custom/listeners/NationXPListener.java
@@ -9,27 +9,26 @@ import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerLevelChangeEvent;
 
 import java.util.Random;
 
 public class NationXPListener extends CustomListener {
 
     @EventHandler
-    public void onAnimalKill(EntityDeathEvent event) {
-        if (!(event.getEntity() instanceof Monster)) return;
-        Monster entity = (Monster) event.getEntity();
-        if (entity.getKiller() == null) return;
-        Player killer = entity.getKiller();
-        Profile killerProfile = instance.getProfile(killer.getUniqueId());
-        if (!killerProfile.isInNation()) return;
+    public void onXPChange(PlayerLevelChangeEvent event) {
+        if (event.getNewLevel() < event.getOldLevel()) return;
+        if (event.getNewLevel() < 30) return;
+        Player player = event.getPlayer();
+        Profile profile = instance.getProfile(player.getUniqueId());
+        if (!profile.isInNation()) return;
         Random r = new Random();
-        double xpToAdd = Methods.round(0.0 + (0.3 - 0.00) * r.nextDouble(), 2);
-        if (xpToAdd == 0.0) return;
-        Nation nation = killerProfile.getCurrentNation();
+        double xpToAdd = Methods.round(0.5 + (1.0 - 0.5) * r.nextDouble(), 2);
+        Nation nation = profile.getCurrentNation();
         nation.setXpPoints(nation.getXpPoints() + xpToAdd);
         nation.save();
-        killer.playSound(killer.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
-        killer.sendActionBar("§b+" + xpToAdd + "§3N-XP");
+        player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+        player.sendActionBar("§b+" + xpToAdd + "§3N-XP");
     }
 
 }
