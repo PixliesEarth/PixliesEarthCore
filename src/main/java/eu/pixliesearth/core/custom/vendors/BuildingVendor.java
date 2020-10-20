8e25288545cf776fd16868ddc0c2c3d05d386590
Amirmahdi Khamisipour <a.khamisipour@outlook.de>
2020-10-20 21:18:53 +0200

    Added Blocks vendor items

--- src/main/java/eu/pixliesearth/core/custom/vendors/BuildingVendor.java
+++ src/main/java/eu/pixliesearth/core/custom/vendors/BuildingVendor.java
@@ -1,12 +1,92 @@
 package eu.pixliesearth.core.custom.vendors;
 
 import eu.pixliesearth.core.vendors.Vendor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import static org.bukkit.Material.*;
 
 public class BuildingVendor extends Vendor {
 
     //TODO ITEMS
     public BuildingVendor() {
-        super("§9Mick the architect", "§9Mick the architect");
+        super("§9Mick the architect", "§9Mick the architect", new ItemStack(DIRT),
+                new ItemStack(GRASS_BLOCK),
+                new ItemStack(PODZOL),
+                new ItemStack(MYCELIUM),
+                new ItemStack(CRIMSON_NYLIUM),
+                new ItemStack(WARPED_NYLIUM),
+                new ItemStack(STRIPPED_ACACIA_LOG),
+                new ItemStack(STRIPPED_BIRCH_LOG),
+                new ItemStack(STRIPPED_JUNGLE_LOG),
+                new ItemStack(STRIPPED_OAK_LOG),
+                new ItemStack(STRIPPED_SPRUCE_LOG),
+                new ItemStack(STRIPPED_DARK_OAK_LOG),
+                new ItemStack(ACACIA_PLANKS),
+                new ItemStack(BIRCH_PLANKS),
+                new ItemStack(CRIMSON_PLANKS),
+                new ItemStack(DARK_OAK_PLANKS),
+                new ItemStack(JUNGLE_PLANKS),
+                new ItemStack(OAK_PLANKS),
+                new ItemStack(SPRUCE_PLANKS),
+                new ItemStack(WARPED_PLANKS),
+                new ItemStack(ACACIA_LOG),
+                new ItemStack(BIRCH_LOG),
+                new ItemStack(JUNGLE_LOG),
+                new ItemStack(OAK_LOG),
+                new ItemStack(SPRUCE_LOG),
+                new ItemStack(DARK_OAK_LOG),
+                new ItemStack(CRIMSON_HYPHAE),
+                new ItemStack(STRIPPED_CRIMSON_HYPHAE),
+                new ItemStack(STRIPPED_WARPED_HYPHAE),
+                new ItemStack(WARPED_HYPHAE),
+                new ItemStack(SPONGE),
+                new ItemStack(SANDSTONE),
+                new ItemStack(RED_SANDSTONE),
+                new ItemStack(BLACK_WOOL),
+                new ItemStack(BLUE_WOOL),
+                new ItemStack(BROWN_WOOL),
+                new ItemStack(CYAN_WOOL),
+                new ItemStack(GRAY_WOOL),
+                new ItemStack(GREEN_WOOL),
+                new ItemStack(LIGHT_BLUE_WOOL),
+                new ItemStack(LIGHT_GRAY_WOOL),
+                new ItemStack(LIME_WOOL),
+                new ItemStack(MAGENTA_WOOL),
+                new ItemStack(ORANGE_WOOL),
+                new ItemStack(PINK_WOOL),
+                new ItemStack(PURPLE_WOOL),
+                new ItemStack(RED_WOOL),
+                new ItemStack(WHITE_WOOL),
+                new ItemStack(YELLOW_WOOL),
+                new ItemStack(QUARTZ),
+                new ItemStack(BRICKS),
+                new ItemStack(STONE_BRICKS),
+                new ItemStack(NETHER_BRICKS),
+                new ItemStack(PRISMARINE),
+                new ItemStack(DARK_PRISMARINE),
+                new ItemStack(PURPUR_BLOCK),
+                new ItemStack(CLAY),
+                new ItemStack(NETHERRACK),
+                new ItemStack(SOUL_SAND),
+                new ItemStack(SOUL_SOIL),
+                new ItemStack(BASALT),
+                new ItemStack(POLISHED_BASALT),
+                new ItemStack(END_STONE),
+                new ItemStack(TERRACOTTA),
+                new ItemStack(WHITE_CONCRETE),
+                new ItemStack(WHITE_CONCRETE_POWDER),
+                new ItemStack(GLASS),
+                new ItemStack(MAGMA_BLOCK),
+                new ItemStack(NETHER_WART_BLOCK),
+                new ItemStack(BLACKSTONE),
+                new ItemStack(POLISHED_BLACKSTONE),
+                new ItemStack(GILDED_BLACKSTONE),
+                new ItemStack(COBBLESTONE),
+                new ItemStack(STONE),
+                new ItemStack(SMOOTH_STONE),
+                new ItemStack(MOSSY_COBBLESTONE)
+                );
     }
 
 }
