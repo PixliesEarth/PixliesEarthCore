303b60efa265f85f12f18a2eb34b7bb1bd859547
Oliver B.R.L <75500268+zenakegit@users.noreply.github.com>
2020-12-06 00:30:59 +0100

    mani recipes :ok_hand:

--- src/main/java/eu/pixliesearth/core/custom/recipes/CarbonKevlarIngot.java
+++ src/main/java/eu/pixliesearth/core/custom/recipes/CarbonKevlarIngot.java
@@ -5,15 +5,15 @@ import eu.pixliesearth.core.custom.CustomRecipe;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ArsenicBronzeIngot extends CustomRecipe {
+public class CarbonKevlarIngot extends CustomRecipe {
 	
-	public ArsenicBronzeIngot() {
+	public CarbonKevlarIngot() {
 		
 	}
-	
+
     @Override
     public String getResultUUID() {
-        return "Pixlies:Arsenic_Bronze_Ingot";
+        return "Pixlies:Carbon_Kevlar_Ingot";
     }
     
     @Override
@@ -29,8 +29,9 @@ public class ArsenicBronzeIngot extends CustomRecipe {
     @Override
     public Map<Integer, String> getContentsList() {
         Map<Integer, String> map = new HashMap<Integer, String>();
-        map.put(0, "Pixlies:Arsenic_Dust");
-        map.put(1, "Pixlies:Copper_Dust");
+       // map.put(0, MinecraftMaterial.OAK_PLANKS.getUUID());
+        map.put(0, "Pixlies:Carbon_Chunk");
+        map.put(0, "Pixlies:Kevlar_Plate");
         return map;
     }
     @Override
