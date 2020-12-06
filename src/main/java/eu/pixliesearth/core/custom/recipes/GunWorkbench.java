afe56573b004854af7a4ef2724256faf9e89b264
Oliver B.R.L <75500268+zenakegit@users.noreply.github.com>
2020-12-06 01:29:01 +0100

    GUNS

---
+++ src/main/java/eu/pixliesearth/core/custom/recipes/GunWorkbench.java
@@ -0,0 +1,33 @@
+package eu.pixliesearth.core.custom.recipes;
+
+import eu.pixliesearth.core.custom.CustomRecipe;
+import eu.pixliesearth.core.custom.MinecraftMaterial;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class GunWorkbench extends CustomRecipe {
+    //@Override
+    public GunWorkbench() {
+        
+    }
+    @Override
+    public String getResultUUID() {
+        return "Machine:Gun_Workbench";
+    }
+    @Override
+    public Map<Integer, String> getContentsList() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(0, "Pixlies:Aluminum_Ingot");
+        map.put(1, "Pixlies:Heavy_Steel_Ingot");
+        map.put(2, "Pixlies:Aluminum_Ingot");
+        map.put(3, "Pixlies:Circuit_Board");
+        map.put(4, "Pixlies:Machine_Base");
+        map.put(5, "Pixlies:Circuit_Board");
+        map.put(6, "Pixlies:Aluminum_Ingot");
+        map.put(7, "Pixlies:Heavy_Steel_Ingot");
+        map.put(8, "Pixlies:Aluminum_Ingot");
+        return map;
+    }
+}
