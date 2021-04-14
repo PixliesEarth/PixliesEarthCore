1cbf4c0f382b8f3dfbb8b113fde80f28e55fe8ce
Amirmahdi Khamisipour <a.khamisipour@outlook.de>
2021-04-15 00:41:55 +0200

    /emoji

---
+++ src/main/java/eu/pixliesearth/core/custom/commands/EmojiCommand.java
@@ -0,0 +1,57 @@
+package eu.pixliesearth.core.custom.commands;
+
+import eu.pixliesearth.core.custom.CustomCommand;
+import eu.pixliesearth.pixliemoji.PixlieMoji;
+import eu.pixliesearth.utils.Methods;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class EmojiCommand extends CustomCommand {
+
+    @Override
+    public String getCommandName() {
+        return "emoji";
+    }
+
+    @Override
+    public String getCommandDescription() {
+        return "Emoji Command";
+    }
+
+    @Override
+    public boolean isPlayerOnlyCommand() {
+        return true;
+    }
+
+    @Override
+    public boolean onExecuted(CommandSender sender, String aliasUsed, String[] parameters, boolean ranByPlayer) {
+        sender.sendMessage(Methods.getCenteredMessage("§8-= §bPixlieMoji Keyboard §8=-"));
+        List<TextComponent> lines = new ArrayList<>();
+        int i = 0;
+        TextComponent currentComponent = new TextComponent();
+        for (Map.Entry<String, Character> entry : PixlieMoji.pixlieMojis.entrySet()) {
+            if (i + 1 > 5) {
+                i = 0;
+                lines.add(currentComponent.duplicate());
+                currentComponent = new TextComponent();
+            }
+            TextComponent currentEmoji = new TextComponent(entry.getValue() + " ");
+            currentEmoji.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click me to paste " + entry.getValue() + " into your chat.")));
+            currentEmoji.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, entry.getValue() + ""));
+            currentComponent.addExtra(currentEmoji);
+            i++;
+        }
+        for (TextComponent line : lines) {
+            sender.spigot().sendMessage(line);
+        }
+        return true;
+    }
+
+}
--- src/main/java/eu/pixliesearth/pixliemoji/PixlieMoji.java
+++ src/main/java/eu/pixliesearth/pixliemoji/PixlieMoji.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class PixlieMoji {
 
-    private static final Map<String, Character> pixlieMojis = new HashMap<String, Character>(){{
+    public static final Map<String, Character> pixlieMojis = new HashMap<String, Character>(){{
         put(":relax:", '是');
         put(":mad:", '了');
         put(":really:", '不');
