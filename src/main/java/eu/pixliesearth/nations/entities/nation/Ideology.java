eddb39ca80ce189a3a354dc366a4bda1f333dd87
Amirmahdi Khamisipour <a.khamisipour@outlook.de>
2021-06-05 06:09:22 +0200

    Funny monkey

--- src/main/java/eu/pixliesearth/nations/entities/nation/Ideology.java
+++ src/main/java/eu/pixliesearth/nations/entities/nation/Ideology.java
@@ -7,13 +7,13 @@ import org.bukkit.Material;
 public enum Ideology {
 
     ANARCHISM('7', Material.BARRIER),
-    COMMUNISM('4', Material.IRON_PICKAXE, "§a§oGains the political power someone else spends from justifying on them"),
+    COMMUNISM('4', Material.IRON_PICKAXE, "§a§oGains the political power someone ", "§a§oelse spends from justifying on them"),
     SOCIAL_DEMOCRACY('c', Material.POPPY, "§a§oTakes 5x as long to justify war on"),
     CONSERVATIVE_DEMOCRACY('9', Material.CORNFLOWER, "§a§o+10% Political Power from XP"),
-    LIBERAL_DEMOCRACY('e', Material.TORCH, "§a§o+0.05 Political power every day per allied nation"),
+    LIBERAL_DEMOCRACY('e', Material.TORCH, "§a§o+0.05 Political power", "§a§o per every allied nation"),
     ABSOLUTE_MONARCHY('6', Material.GOLDEN_HELMET),
     FASCISM('3', Material.WITHER_ROSE, "§a§o2x Faster Justification"),
-    THEOCRACY('2', Material.BOOK, "§a§o15% Cheaper Justification on people who are a different religion"),
+    THEOCRACY('2', Material.BOOK, "§a§o15% Cheaper Justification on people", "§a§o who are a different religion"),
     CONSTITUTIONAL_MONARCHY('3', Material.PAPER),
     DICTATORSHIP('5', Material.NETHERITE_SWORD, "§a§o25% more political power"),
     TRIBE('f', Material.CAMPFIRE),
