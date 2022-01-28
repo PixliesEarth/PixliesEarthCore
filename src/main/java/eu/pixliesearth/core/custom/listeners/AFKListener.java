373edcba64d876d73a0134634a0805ef341b432e
dynmie <xbottam@gmail.com>
2022-01-28 14:35:11 -0800

    vanished players no longer broadcast afk status

--- src/main/java/eu/pixliesearth/core/custom/listeners/AFKListener.java
+++ src/main/java/eu/pixliesearth/core/custom/listeners/AFKListener.java
@@ -1,18 +1,25 @@
 package eu.pixliesearth.core.custom.listeners;
 
+import eu.pixliesearth.Main;
 import eu.pixliesearth.core.custom.CustomListener;
+import eu.pixliesearth.localization.Lang;
 import net.pl3x.purpur.event.PlayerAFKEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
 public class AFKListener extends CustomListener {
 
+    private static final Main instance = Main.getInstance();
+
     @EventHandler
     public void handleAFK(PlayerAFKEvent event) {
         event.setShouldKick(false);
+        Player player = event.getPlayer();
+        if (instance.getUtilLists().vanishList.contains(player.getUniqueId())) return;
         if (event.isGoingAfk()) {
-            event.setBroadcastMsg("§a§lEARTH §8| §6" + event.getPlayer().getDisplayName() + " §7is now AFK.");
+            event.setBroadcastMsg(Lang.EARTH + "§6" + player.getDisplayName() + " §7is now AFK.");
         } else {
-            event.setBroadcastMsg("§a§lEARTH §8| §6" + event.getPlayer().getDisplayName() + " §7is back.");
+            event.setBroadcastMsg(Lang.EARTH + "§6" + player.getDisplayName() + " §7is back.");
         }
     }
 
